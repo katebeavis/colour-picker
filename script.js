9753465d0b83fec4c3f8ebaf80ab7f3acc896f76
katebeavis <katebeavis84@gmail.com>
2015-05-18 18:05:55 +0100

    Can pick colour by typing into input box

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 /* Exercise 2: Color picker */
+ $(document).on('keyup keydown keypress', '#color', function() {
+    setPreviewColor($("input").val());
+ });
+
+function setPreviewColor(color) {
+    $(".preview").css("background-color",color);
+}

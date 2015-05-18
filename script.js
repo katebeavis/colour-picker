828ea51bf55b60d6e3cea1183ca8d87676416be5
katebeavis <katebeavis84@gmail.com>
2015-05-18 18:14:50 +0100

    Added rgb color display below colour box

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 /* Exercise 2: Color picker */
  $(document).on('keyup keydown keypress', '#color', function() {
+
     setPreviewColor($("input").val());
  });
 
 function setPreviewColor(color) {
+
     $(".preview").css("background-color",color);
+
+    var rgb = $(".preview").css("background-color");
+
+    $(".color-code").text(rgb);
 }

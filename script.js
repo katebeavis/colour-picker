322e973e6f6e67f49fb1b7ba1113fb9effedf883
katebeavis <katebeavis84@gmail.com>
2015-05-19 12:29:32 +0100

    tidied up indentation and refactored

--- script.js
+++ script.js
@@ -1,91 +1,41 @@
 /* Exercise 2: Color picker */
+var colors = ["22ac5e", "d68236", "58ACFA", "58D3F7", "BF00FF", "yellow", "pink", "indigo", "blue", "turquoise" ];
+
 function setPreviewColor(color) {
-
-  $(".preview").css("background-color",color);
-
+  $(".preview").css("background-color", color);
   $(".color-code").text($(".preview").css("background-color"));
 }
 
-$(document).ready(function(){
-
+$(document).ready(function() {
   $.each(colors, function(index, element) {
-
     addBox(element);
-
   });
 
-    var $preview = $(".preview");
-
-    var randomPosition = Math.floor( Math.random() * colors.length );
-
-    $preview.css("background-color",colors[randomPosition]);
+  var $preview = $(".preview");
+  var randomPosition = Math.floor(Math.random() * colors.length);
+  $preview.css("background-color", colors[randomPosition]);
 
   $(document).on('keyup keydown keypress', '#color', function() {
-
     color = setPreviewColor($("input").val());
-
   });
 
   $(document).on("click", "button#add-to-favorite", function() {
-
     color = $("input").val();
-
     addBox(color);
-
     $("input").val("").focus();
   });
 
-  $(document).on('mouseenter', "#colors .item",  function(){
-
+  $(document).on('mouseenter', "#colors .item", function() {
     previewColour = $preview.css("background-color");
-
     setPreviewColor($(this).css("background-color"));
-
   }).on('mouseleave', "#colors .item", function() {
-
-    $preview.css("background-color",previewColour);
-
+    $preview.css("background-color", previewColour);
   });
 });
 
 function addBox(color) {
-
-  if (boxNumber() === false) {
-
-    removeBox();
+  if ($("#colors .item").length == 16) {
+    $("#colors .item:last-child").remove();
   }
-
-    square = "<div class='item' style='background-color: " + color + ";'></div>";
-
-    $("div#colors").prepend(square);
-
+  $("div#colors").prepend("<div class='item' style='background-color: " + color + ";'></div>");
 }
-
-function boxNumber() {
-
-  favCount = $("#colors .item").length;
-
-  if (favCount < 16) {
-
-    return true;
-
-  } else {
-
-    return false;
-  }
-
-}
-
-function removeBox() {
-
-  var lastBox = $("#colors .item:last-child");
-
-  lastBox.remove();
-}
-
-var colors = [ "22ac5e", "d68236", "58ACFA", "58D3F7", "BF00FF", "yellow", "pink", "indigo", "blue", "turquoise" ];
-
-
-
-
-
